520a9ceb1f16503ed729ced6cee13a40f8003381
Planeshifter <pburckhardt@outlook.com>
2016-02-19 22:49:41 -0500

    Move generator tests to separate file

--- test/test.js
+++ test/test.js
@@ -8,6 +8,11 @@ var tape = require( 'tape' );
 var sumSeries = require( './../lib/' );
 
 
+// VARIABLES //
+
+var hasGeneratorsSupport = require( 'detect-generator-support' )();
+
+
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
@@ -35,6 +40,12 @@ tape( 'function throws an error if provided an invalid option', function test( t
 	}
 });
 
+// Run generator function tests if environment supports `function*()`...
+
+if ( hasGeneratorsSupport ) {
+	require( './es2015/test.generator.js' );
+}
+
 tape( 'the function calculates the sum of an infinite series provided by a closure', function test( t ) {
 	// log1p( 0.5 ):
 	var actual = sumSeries( closure( 0.5 ) );
@@ -54,40 +65,21 @@ tape( 'the function calculates the sum of an infinite series provided by a closu
 	}
 });
 
-tape( 'the function calculates the sum of an infinite series provided by a generator', function test( t ) {
-	// log1p( 0.5 ):
-	var actual = sumSeries( generator( 0.5 ) );
-	var expected = log1p( 0.5 );
-
-	t.ok( abs( actual - expected ) < 1e-14, 'returned result is within tolerance. actual: ' + actual + '; expected: ' + expected + '.' );
-	t.end();
-
-	function* generator( x ) {
-		var k = 0;
-		var m_mult = -x;
-		var m_prod = -1;
-		while ( true ) {
-			m_prod *= m_mult;
-			yield ( m_prod / ++k );
-		}
-	}
-});
-
 tape( 'the function calculates the sum of an infinite series with a specified initial value', function test( t ) {
 	// log1p( 0.5 ) + 2:
-	var actual = sumSeries( generator( 0.5 ), { 'init': 2 } );
+	var actual = sumSeries( closure( 0.5 ), { 'init': 2 } );
 	var expected = log1p( 0.5 ) + 2;
 
 	t.ok( abs( actual - expected ) < 1e-14, 'returned result is within tolerance. actual: ' + actual + '; expected: ' + expected + '.' );
 	t.end();
 
-	function* generator( x ) {
+	function closure( x ) {
 		var k = 0;
 		var m_mult = -x;
 		var m_prod = -1;
-		while ( true ) {
+		return function() {
 			m_prod *= m_mult;
-			yield ( m_prod / ++k );
-		}
+			return ( m_prod / ++k );
+		};
 	}
 });
